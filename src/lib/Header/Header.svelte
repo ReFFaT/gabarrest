cf12fc8f5f5d4f925af37ec7a19831ddb0bfa7d3
elbrus <refot.ref@mail.ru>
2024-03-05 23:30:53 +0300

    fix

---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,128 @@
+<script>
+	import { onMount } from "svelte";
+    import "./Header.css"
+
+    let activeRoute='home'
+    let routeMove;
+    let isAdmin=false
+    function setActiveRoute(route){
+        activeRoute=route
+        let newRoute = document.getElementById(route+"-header")
+        let newRouteRect = newRoute.getBoundingClientRect();
+
+        routeMove.style.left = newRouteRect.left + 10 - routeMove.getBoundingClientRect().width  + "px"
+    }
+    onMount(()=>{
+        setActiveRoute("home")
+    })
+
+</script>
+<div class="header">
+    <div class="header__wrapper">
+        <ul class="header__list">
+            <li class=" header__item-wrapper" id="home-header">
+                <a href="/home" class="header__list-item {activeRoute==="home" && "active"}"
+                on:click={()=>setActiveRoute("home")}>
+                    <img src="/images/home-outline.svg" class="header__list-icon" alt="">
+                    <p class="header__list-text">Главная</p>
+                </a>
+            </li>
+            <li class="header__item-wrapper" id="menu-header">
+                <a href="/menu" class="header__list-item {activeRoute==="menu" && "active"}"
+                on:click={()=>setActiveRoute("menu")}>
+                <img src="/images/book-outline.svg" class="header__list-icon" alt="">
+                <p class="header__list-text">Меню</p>
+                </a>
+            </li>
+            <li class="header__item-wrapper" id="info-header">
+                <a  href="/info" class="header__list-item {activeRoute==="info" && "active"}"
+                on:click={()=>setActiveRoute("info")}>
+                    <img src="/images/basket-outline.svg" class="header__list-icon" alt="">
+                    <p class="header__list-text">Корзина</p>
+                </a>
+            </li>
+            <li class="header__item-wrapper" id="order-header">
+                <a href="/order" class="header__list-item {activeRoute==="order" && "active"}"
+                on:click={()=>setActiveRoute("order")}
+                >
+                    <img src="/images/ice-cream-outline.svg" class="header__list-icon" alt="">
+                    <p class="header__list-text">Заказы</p>
+                </a>
+            </li>
+        </ul>
+        <div bind:this={routeMove} class="header__slider"></div>
+        <button class="header__button-login">
+            Регистрация
+        </button>
+        <button class="header__button-login">
+            Вход
+        </button>
+        
+    </div>
+    {#if isAdmin}
+        <div class="header__admin">
+            <div class="header__admin-settings">
+                <button class="header__admin-settings-btn" >
+                    <img class="header__admin-settings-btn-image" src="images/settings.svg" alt="">
+                </button>
+            </div>
+            <div class="header__admin-dashboard">
+                <a class="header__admin-dashboard-href" href="/dashBoard" >
+                    <img class="header__admin-dashboard-image" src="images/dashboard.svg" alt="">
+                </a>   
+            </div>        
+        </div> 
+    {/if}
+</div>
+
+
+<style>
+    .header__admin{
+        right: 0;
+        position: relative;
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
+    .header__admin-settings{
+        width: 50px;
+        height: 50px;
+    }
+    .header__admin-settings-btn-image{
+        width: 36px;
+        height: 36px;
+    }
+    .header__admin-settings-btn{
+        background-color: white;
+        border-radius: 10px;
+        height: 50px;
+        width: 50px;
+        border: none;
+        transition: all .3s;
+        &:hover{
+            transform: scale(1.1);
+        }
+        &:active{
+            transform: scale(0.9);
+        }
+    }
+    .header__admin-dashboard{
+        background-color: white;
+        border-radius: 10px;
+        height: 50px;
+        width: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        &:hover{
+            transform: scale(1.1);
+        }
+        &:active{
+            transform: scale(0.9);
+        }    
+    }
+    .header__admin-dashboard-image{
+        width: 36px;
+        height: 36px;
+    }
+</style>
